ffbfd608b6511eadcb830b67b156201bd3800e23
dw2023 <dwkim126@gmail.com>
2023-04-03 17:37:35 +0900

    회원가입 처리

--- src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
+import com.ll.gramgram.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -7,20 +8,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
     public String showJoin() {
         return "usr/member/join";
     }
-
     @AllArgsConstructor
     @Getter
     public static class JoinForm {
@@ -34,6 +34,8 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
+        memberService.join(joinForm.getUsername(), joinForm.getPassword());
+
         return "redirect:/";
     }
 }
