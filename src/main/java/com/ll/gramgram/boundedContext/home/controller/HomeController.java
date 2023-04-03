678e316313460fe766e772da6401eb7dd0f32f44
dw2023 <dwkim126@gmail.com>
2023-04-04 08:53:30 +0900

    23강, 로그인 후에 자신의 번호와 가입날짜를 내비바에서 볼 수 있도록, 무식한 방법으로 해결

--- src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.ll.gramgram.boundedContext.home.controller;
 
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import com.ll.gramgram.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
 import java.util.Enumeration;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final MemberService memberService;
+
     @GetMapping("/")
-    public String showMain() {
+    public String showMain(Model model, Principal principal) {
+        if ( principal != null ) {
+            Member loginedMember = memberService.findByUsername(principal.getName()).orElseThrow();
+            model.addAttribute("loginedMember", loginedMember);
+        }
+
         return "usr/home/main";
     }
 
@@ -20,14 +31,12 @@ public class HomeController {
     @ResponseBody
     public String showDebugSession(HttpSession session) {
         StringBuilder sb = new StringBuilder("Session content:\n");
-
         Enumeration<String> attributeNames = session.getAttributeNames();
         while (attributeNames.hasMoreElements()) {
             String attributeName = attributeNames.nextElement();
             Object attributeValue = session.getAttribute(attributeName);
             sb.append(String.format("%s: %s\n", attributeName, attributeValue));
         }
-
-        return sb.toString().replaceAll("\n", "<br>"); // 화면에 잘 보이도록 줄바꿈
+        return sb.toString().replaceAll("\n", "<br>");
     }
 }
